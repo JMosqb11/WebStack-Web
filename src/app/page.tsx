3e3d62a9a9bea714011f6e770af7dbf5be56c3ac
jhoniermosquera61 <jhoniermosquera61@gmail.com>
2024-03-12 15:41:51 +0000

    creation of warning for my portafolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,17 @@ import React from "react";
 import Banner from "@/containers/Banner/Banner";
 import Contacto from "@/containers/Contacto/Contacto";
 import Footer from "@/containers/Footer/Footer";
+import { Alert, Stack } from "@mui/material";
 
 export default function Home() {
 
   return (
     <>
+    <section>
+    <Stack sx={{ width: '100%' }} spacing={2}>
+      <Alert variant="filled" severity="warning">Este sitio se encuentra en construcción, usted está previsualizando la versión BETA</Alert>
+    </Stack>
+    </section>
       <section>
         <Banner/>
         <Contacto/>
